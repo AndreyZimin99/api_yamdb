cfb7580ff3ae412ba0450bf73a869775ee971c76
Andrey Zimin <andrey.zimin1999@gmail.com>
2024-10-16 21:31:02 +0300

    fixing titles ang set

--- api_yamdb/titles/models.py
+++ api_yamdb/titles/models.py
@@ -31,7 +31,7 @@ class Genre(models.Model):
 class Title(models.Model):
     name = models.CharField('Имя произведения', max_length=256)
     year = models.SmallIntegerField('Год выпуска', validators=[valid_date])
-    description = models.CharField('Описание')
+    description = models.TextField('Описание')
     genre = models.ManyToManyField(Genre, related_name='titles',
                                    verbose_name='Жанр')
     category = models.ForeignKey(Category, related_name='titles',
