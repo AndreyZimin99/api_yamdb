9f6c01dea4f92b9c9f46676f668fc577df66eadf
Andrey Zimin <andrey.zimin1999@gmail.com>
2024-10-25 10:12:36 +0300

    импорты

--- api_yamdb/titles/management/commands/load_data.py
+++ api_yamdb/titles/management/commands/load_data.py
@@ -1,8 +1,9 @@
 import csv
 from django.core.management.base import BaseCommand
+
+from reviews.models import Comment, Review
+from titles.models import Category, Genre, GenreTitle, Title
 from users.models import User
-from titles.models import Category, Genre, Title, GenreTitle
-from reviews.models import Review, Comment
 
 
 class Command(BaseCommand):
